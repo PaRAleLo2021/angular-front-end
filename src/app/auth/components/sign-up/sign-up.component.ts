80721225e83d9fe9640c402cd14e061e5ae8cb14
alexandra-doris <62300754+alexandra-doris@users.noreply.github.com>
2021-05-26 19:54:04 +0300

    Add allerts for Register

--- src/app/auth/components/sign-up/sign-up.component.ts
+++ src/app/auth/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { UserService } from 'src/app/_include/user.service';
 
 @Component({
@@ -7,6 +8,9 @@ import { UserService } from 'src/app/_include/user.service';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  message: string = "";
+  isMessage = false;
+  isError = false;
 
   constructor(private userService: UserService) { }
 
@@ -14,8 +18,17 @@ export class SignUpComponent implements OnInit {
   }
 
   register(username: string, email: string, pws: string) {
-    this.userService.SignUp(username, email, pws).subscribe((response: any) => {
-      console.log(response)
+   this.userService.SignUp(username, email, pws).subscribe(data => {
+      let data2: any = data;
+      this.message = `User ${data2["user"]["username"]}, added successfuly`;
+      this.isMessage = true;
+      this.isError = false;
+      console.log("This is the response: "+ data2)
+    }, err => {
+      this.message = err["error"]["message"];
+      this.isMessage = false;
+      this.isError = true;
+      console.log("This is the response: "+ err)
     });
   }
 
